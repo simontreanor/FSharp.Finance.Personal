6fb85393dfc11dc090d13c40c16be72caac53e61
Simon Treanor <simon.treanor@fundourselves.com>
2024-02-06 16:56:36 +0000

    backwards compatible

--- docs/exampleAmortisation.fsx
+++ docs/exampleAmortisation.fsx
@@ -73,8 +73,8 @@ It is possible to format the `Items` property as an HTML table:
 
 let html =
     amortisationSchedule
-    |> ValueOption.map (_.ScheduleItems >> Formatting.generateHtmlFromArray)
+    |> ValueOption.map (fun s -> s.ScheduleItems |> Formatting.generateHtmlFromArray)
     |> ValueOption.defaultValue "[could not format]"
 html
 
-(*** include-it ***)
+(*** include-it-raw ***)
