e56453f1e63db52d5f7f5ddaea575234e2da5ca3
Simon Treanor <simon.treanor@fundourselves.com>
2024-09-09 23:05:11 +0100

    split formatter so output can be used earlier

--- src/Formatting.fs
+++ src/Formatting.fs
@@ -9,19 +9,7 @@ module Formatting =
     open Microsoft.FSharp.Reflection
     open System.Text.RegularExpressions
 
-    open Calculation
     open Currency
- 
-    /// an array of properties relating to (product) fees
-    let feesProperties hide = if hide then [| "FeesPortion"; "FeesRefund"; "FeesBalance"; "FeesRefundIfSettled" |] else [||]
-    /// an array of properties relating to (penalty) charges
-    let chargesProperties hide = if hide then [| "NewCharges"; "ChargesPortion"; "ChargesBalance" |] else [||]
-    /// an array of properties relating to quotes
-    let quoteProperties hide = if hide then [| "GeneratedPayment" |] else [||]
-    /// an array of properties representing extra information
-    let extraProperties hide = if hide then [| "FeesRefundIfSettled"; "SettlementFigure"; "Window" |] else [||]
-    /// an array of properties representing internal interest calculations
-    let interestProperties hide = if hide then [| "ContractualInterest"; "SimpleInterest" |] else [||]
 
     /// filter out hidden fields 
     let filterColumns hideProperties =
@@ -103,31 +91,8 @@ module Formatting =
         )
         |> String.concat ""
 
-    /// a set of options specifying which fields to show/hide in the output
-    type GenerationOptions = {
-        GoParameters: PaymentSchedule.Parameters
-        GoPurpose: IntendedPurpose
-        GoExtra: bool
-    }
-
-    /// determines which fields to hide
-    let getHideProperties generationOptions =
-        match generationOptions with
-        | Some go ->
-            [|
-                go.GoParameters.FeesAndCharges.Fees |> Array.isEmpty |> feesProperties
-                go.GoParameters.FeesAndCharges.Charges |> Array.isEmpty |> chargesProperties
-                (match go.GoPurpose with IntendedPurpose.Settlement _ -> false | _ -> true) |> quoteProperties
-                (match go.GoParameters.Interest.Method with Interest.Method.AddOn -> false | _ -> true) |> interestProperties
-                go.GoExtra |> not |> extraProperties
-            |]
-            |> Array.concat
-        | None ->
-            [||]
-
     /// generates a formatted HTML table from an array
-    let generateHtmlFromArray options (items: 'a array) =
-        let hideProperties = options |> getHideProperties
+    let generateHtmlFromArray hideProperties (items: 'a array) =
         let propertyInfos = typeof<'a> |> FSharpType.GetRecordFields
         let header = propertyInfos |> formatHtmlTableHeader hideProperties
         let rows = items |> formatHtmlTableRows hideProperties propertyInfos
@@ -136,6 +101,5 @@ module Formatting =
     /// legacy function for creating HTML files from enumerables
     let outputListToHtml fileName list =
         list
-        |> generateHtmlFromArray None
+        |> generateHtmlFromArray [||]
         |> outputToFile' fileName
-
---
+++ src/FormattingHelper.fs
@@ -0,0 +1,39 @@
+namespace FSharp.Finance.Personal
+
+/// convenience module for generating HTML tables, optimised for amortisation schedules
+module FormattingHelper =
+
+    open Calculation
+ 
+    /// an array of properties relating to (product) fees
+    let feesProperties hide = if hide then [| "FeesPortion"; "FeesRefund"; "FeesBalance"; "FeesRefundIfSettled" |] else [||]
+    /// an array of properties relating to (penalty) charges
+    let chargesProperties hide = if hide then [| "NewCharges"; "ChargesPortion"; "ChargesBalance" |] else [||]
+    /// an array of properties relating to quotes
+    let quoteProperties hide = if hide then [| "GeneratedPayment" |] else [||]
+    /// an array of properties representing extra information
+    let extraProperties hide = if hide then [| "FeesRefundIfSettled"; "SettlementFigure"; "Window" |] else [||]
+    /// an array of properties representing internal interest calculations
+    let interestProperties hide = if hide then [| "ContractualInterest"; "SimpleInterest" |] else [||]
+
+    /// a set of options specifying which fields to show/hide in the output
+    type GenerationOptions = {
+        GoParameters: PaymentSchedule.Parameters
+        GoPurpose: IntendedPurpose
+        GoExtra: bool
+    }
+
+    /// determines which fields to hide
+    let getHideProperties generationOptions =
+        match generationOptions with
+        | Some go ->
+            [|
+                go.GoParameters.FeesAndCharges.Fees |> Array.isEmpty |> feesProperties
+                go.GoParameters.FeesAndCharges.Charges |> Array.isEmpty |> chargesProperties
+                (match go.GoPurpose with IntendedPurpose.Settlement _ -> false | _ -> true) |> quoteProperties
+                (match go.GoParameters.Interest.Method with Interest.Method.AddOn -> false | _ -> true) |> interestProperties
+                go.GoExtra |> not |> extraProperties
+            |]
+            |> Array.concat
+        | None ->
+            [||]
