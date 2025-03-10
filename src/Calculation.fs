7f3f4be1fa14b8a8ac38e1220128caff8339a4c1
Simon Treanor <4550815+simontreanor@users.noreply.github.com>
2025-03-10 12:00:05 +0000

    remove tailcall attribute until F# support is added

--- src/Calculation.fs
+++ src/Calculation.fs
@@ -288,7 +288,6 @@ module Calculation =
     module Array =
         /// iteratively solves for a given input using a generator function until the output hits zero or within a set tolerance,
         /// optionally relaxing the tolerance until a solution is found
-        [<TailCall>]
         let solveBisection (generator: decimal -> decimal) (iterationLimit: uint) initialGuess targetTolerance (toleranceSteps: ToleranceSteps) =
             let initialLowerBound, initialUpperBound = initialGuess * 0.95m, initialGuess * 1.05m
             // recursively iteration through possible solutions
